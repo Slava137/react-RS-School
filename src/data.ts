2110325fa443cff64bae9568b42e63d4f6ea12c6
Slava137 <slava13071995@gmail.com>
2023-04-09 00:43:55 +0300

    feat: add a fake data for project

---
+++ src/data.ts
@@ -0,0 +1,174 @@
+export interface CardData {
+  id: number;
+  title: string;
+  description: string;
+  price: number;
+  rating: number;
+  brand: string;
+  category: string;
+  photo: string;
+}
+
+const dataProducts: CardData[] = [
+  {
+    id: 1,
+    title: 'MacBook Pro',
+    description: 'MacBook Pro 2021 with mini-LED display may launch between September, November',
+    price: 1749,
+    rating: 4.57,
+    brand: 'Apple',
+    category: 'laptops',
+    photo: 'https://i.dummyjson.com/data/products/6/thumbnail.png',
+  },
+  {
+    id: 2,
+    title: 'Samsung Galaxy Book',
+    description:
+      'Samsung Galaxy Book S (2020) Laptop With Intel Lakefield Chip, 8GB of RAM Launched',
+    price: 1499,
+    rating: 4.25,
+    brand: 'Samsung',
+    category: 'laptops',
+    photo: 'https://i.dummyjson.com/data/products/7/thumbnail.jpg',
+  },
+  {
+    id: 3,
+    title: 'Microsoft Surface Laptop 4',
+    description:
+      'Style and speed. Stand out on HD video calls backed by Studio Mics. Capture ideas on the vibrant touchscreen.',
+    price: 1499,
+    rating: 4.43,
+    brand: 'Microsoft Surface',
+    category: 'laptops',
+    photo: 'https://i.dummyjson.com/data/products/8/thumbnail.jpg',
+  },
+  {
+    id: 4,
+    title: 'Infinix INBOOK',
+    description: 'Infinix Inbook X1 Ci3 10th 8GB 256GB 14 Win10 Grey – 1 Year Warranty',
+    price: 1099,
+    rating: 4.54,
+    brand: 'Infinix',
+    category: 'laptops',
+    photo: 'https://i.dummyjson.com/data/products/9/thumbnail.jpg',
+  },
+  {
+    id: 5,
+    title: 'HP Pavilion 15-DK1056WM',
+    description:
+      'HP Pavilion 15-DK1056WM Gaming Laptop 10th Gen Core i5, 8GB, 256GB SSD, GTX 1650 4GB, Windows 10',
+    price: 1099,
+    rating: 4.43,
+    brand: 'HP Pavilion',
+    category: 'laptops',
+    photo: 'https://i.dummyjson.com/data/products/10/thumbnail.jpeg',
+  },
+  {
+    id: 6,
+    title: 'iPhone 9',
+    description: 'An apple mobile which is nothing like apple',
+    price: 549,
+    rating: 4.69,
+    brand: 'Apple',
+    category: 'smartphones',
+    photo: 'https://i.dummyjson.com/data/products/1/thumbnail.jpg',
+  },
+  {
+    id: 7,
+    title: 'iPhone X',
+    description:
+      'SIM-Free, Model A19211 6.5-inch Super Retina HD display with OLED technology A12 Bionic chip with ...',
+    price: 899,
+    rating: 4.44,
+    brand: 'Apple',
+    category: 'smartphones',
+    photo: 'https://i.dummyjson.com/data/products/2/thumbnail.jpg',
+  },
+  {
+    id: 8,
+    title: 'Samsung Universe 9',
+    description: "Samsung's new variant which goes beyond Galaxy to the Universe",
+    price: 1249,
+    rating: 4.09,
+    brand: 'Samsung',
+    category: 'smartphones',
+    photo: 'https://i.dummyjson.com/data/products/3/thumbnail.jpg',
+  },
+  {
+    id: 9,
+    title: 'OPPO F19',
+    description: 'OPPO F19 is officially announced on April 2021.',
+    price: 280,
+    rating: 4.3,
+    brand: 'OPPO',
+    category: 'smartphones',
+    photo: 'https://i.dummyjson.com/data/products/4/thumbnail.jpg',
+  },
+  {
+    id: 10,
+    title: 'Huawei P30',
+    description:
+      "Huawei's re-badged P30 Pro New Edition was officially unveiled yesterday in Germany and now the device has made its way to the UK.",
+    price: 499,
+    rating: 4.09,
+    brand: 'Huawei',
+    category: 'smartphones',
+    photo: 'https://i.dummyjson.com/data/products/5/thumbnail.jpg',
+  },
+  {
+    id: 11,
+    title: 'Black Motorbike',
+    description:
+      'Engine Type:Wet sump, Single Cylinder, Four Stroke, Two Valves, Air Cooled with SOHC (Single Over Head Cam) Chain Drive Bore & Stroke:47.0 x 49.5 MM',
+    price: 569,
+    rating: 4.04,
+    brand: 'METRO 70cc - MR70',
+    category: 'motorcycle',
+    photo: 'https://i.dummyjson.com/data/products/91/thumbnail.jpg',
+  },
+  {
+    id: 12,
+    title: 'Hot sale in europe motorcycle',
+    description:
+      'Hot sale in europe electric racing motorcycle electric motorcycle for sale adult electric motorcycles',
+    price: 920,
+    rating: 4.19,
+    brand: 'BRAVE BULL',
+    category: 'motorcycle',
+    photo: 'https://i.dummyjson.com/data/products/92/thumbnail.jpg',
+  },
+  {
+    id: 13,
+    title: 'Automatic Motor Gas Motorcycles',
+    description:
+      '150cc 4-Stroke Motorcycle Automatic Motor Gas Motorcycles Scooter motorcycles 150cc scooter',
+    price: 1050,
+    rating: 4.84,
+    brand: 'shock absorber',
+    category: 'motorcycle',
+    photo: 'https://i.dummyjson.com/data/products/93/thumbnail.jpg',
+  },
+  {
+    id: 14,
+    title: 'New arrivals Fashion motocross goggles',
+    description: 'New arrivals Fashion motocross goggles motorcycle motocross racing motorcycle',
+    price: 900,
+    rating: 4.06,
+    brand: 'JIEPOLLY',
+    category: 'motorcycle',
+    photo: 'https://i.dummyjson.com/data/products/94/thumbnail.webp',
+  },
+  {
+    id: 15,
+    title: 'Wholesale cargo lashing Belt',
+    description:
+      'Wholesale cargo lashing Belt Tie Down end Ratchet strap customized strap 25mm motorcycle 1500kgs with rubber handle',
+    price: 930,
+    rating: 4.21,
+    brand: 'Xiangle',
+    category: 'motorcycle',
+    photo: 'https://i.dummyjson.com/data/products/95/thumbnail.jpg',
+  },
+];
+
+export default dataProducts;
